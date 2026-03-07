d5c77720ce87998218818386c05817e30a73a528
Chamala Venkata Rithish Reddy <rithishreddychamala@gmail.com>
2026-03-07 12:53:59 +0530

    deleted some useless files

--- server/test_pdf2.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const pdfLib = require('pdf-lib');
-
-(async () => {
-    // pdf-lib does not easily allow extracting text coordinates natively because it focuses on generating.
-    // Let's use pdf2json or pdf-parse.
-    console.log("We'll use standard layout based on trial and error");
-})();
